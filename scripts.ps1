0a162bea1d6ca1165e4435f8896784402575961f
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-07 14:14:33 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -2,27 +2,33 @@
 $github_token = $env:github_token
 $access_token = $env:access_token
 
-$headers = @{Authorization = "Bearer $access_token"}
+$headers = @{ Authorization = "Bearer $access_token" }
 
-# create cicd folder
-if(!(test-path -PathType container cicd)){
-      mkdir "cicd"
-      cd cicd
-      Write-Host "inside 2nd if"
-}
-else {
-      cd cicd
-      Write-Host "2nd else"
+# create cicd folder if not exists
+$cicdPath = "cicd"
+if (!(Test-Path -PathType Container $cicdPath)) {
+    mkdir $cicdPath
+    Set-Location -Path $cicdPath
+    Write-Host "Inside if: Created and moved to $cicdPath"
+} else {
+    Set-Location -Path $cicdPath
+    Write-Host "Inside else: Moved to $cicdPath"
 }
 
+# API request
 $path = "https://www.workato.com/api/packages/export/101814"
-$proxies = Invoke-RestMethod -Uri $path -Method 'POST' -Headers $headers -ContentType "application/json" -ErrorAction:Stop -TimeoutSec 60
+try {
+    $response = Invoke-RestMethod -Uri $path -Method 'POST' -Headers $headers -ContentType "application/json" -ErrorAction Stop -TimeoutSec 60
+    Write-Host "API Request Successful"
+    
+    # Convert JSON data to PowerShell object
+    $dataObject = $response | ConvertFrom-Json
 
-# Convert JSON data to PowerShell object
-$dataObject = $proxies | ConvertFrom-Json
+    # Extract the "id" value
+    $idValue = $dataObject.id
 
-# Extract the "id" value
-$idValue = $dataObject.id
-
-# Print the result
-Write-Host "ID Value: $idValue"
+    # Print the result
+    Write-Host "ID Value: $idValue"
+} catch {
+    Write-Host "API Request Failed. Error: $_"
+}
