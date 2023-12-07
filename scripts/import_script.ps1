f8875e7d1a3ead0ca43c0390dea3a502935883df
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-07 23:12:00 +0530

    Create import_script.ps1

---
+++ scripts/import_script.ps1
@@ -0,0 +1,6 @@
+# Input parameters
+Param (
+    [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token
+    [Parameter(mandatory = $true)][string]$folderId, # To receive folderId  
+    [Parameter(mandatory = $true)][string]$manifestName # To receive manifest Name  
+)
