08233b1b3f3621e977680cda45b3504ba99dbe14
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-07 15:36:24 +0530

    Update scripts.ps1

--- scripts.ps1
+++ scripts.ps1
@@ -48,17 +48,17 @@ try {
                 if ($downloadURL -ne $null -and $downloadURL -ne "null") {
                     # Write-Host "Download URL obtained: $downloadURL"
 
-                    # Extract file name from the URL
-                    $fileName = [System.IO.Path]::GetFileName($downloadURL)
-                
+                    # Extract file name from the URL without query parameters
+                    $fileName = [System.IO.Path]::GetFileNameWithoutExtension($downloadURL)
+                    
                     # Set the path where you want to save the file (inside the cicd folder)
-                    $savePath = Join-Path $PSScriptRoot "cicd\$fileName"
-                
+                    $savePath = Join-Path $PSScriptRoot "cicd\$fileName.zip"
+                    
                     Write-Host "Downloading file to: $savePath"
-                
+                    
                     # Download the file
                     Invoke-WebRequest -Uri $downloadURL -OutFile $savePath
-                
+                    
                     Write-Host "File downloaded successfully!"
                 }
             } else {
