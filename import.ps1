56f74d6e0432ca29fb37adef07fee0444eb461ad
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-08 10:46:16 +0530

    Update import.ps1

--- import.ps1
+++ import.ps1
@@ -1,18 +1,16 @@
-
 # Input parameters
 Param (
     [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token  
     [Parameter(mandatory = $true)][string]$manifestName, # To receive manifest Name
     [Parameter(mandatory = $true)][string]$folderId # To receive folderId
 )
-Write-Host "manifestName:$manifestName"
 
+Write-Host "manifestName:$manifestName"
 Write-Host "folderId:$folderId"
 
-$headers = @{Authorization = "Bearer $accessToken " }
+$headers = @{Authorization = "Bearer $accessToken" }
 
 $manifestDirectory = "cicd"
-
 Write-Host "manifestDirectory:$manifestDirectory"
 
 Write-Host "Start Import manifest for $manifestName "
@@ -22,14 +20,16 @@ $manifestNameFolder = "$currentdir"
 
 # https://www.workato.com/api/packages/import/#{_('data.lookup_table.f64a4d0d.entry.col3')}?restart_recipes=true
 
-if ($manifestName -ne 'null' -AND (Test-Path $manifestNameFolder )) {
+if ($manifestName -ne 'null' -AND (Test-Path $manifestNameFolder)) {
     Set-Location $manifestNameFolder
     $zipFile = Get-ChildItem -Filter "$manifestName.zip"
 
     if ($zipFile) {
-        $requestFile = @{file = Get-Item -Path $zipFile.FullName }
+        $zipContent = [Convert]::ToBase64String([IO.File]::ReadAllBytes($zipFile.FullName))
+        $requestFile = @{ file = $zipContent; filename = $zipFile.Name }
+        
         $uri = "https://www.workato.com/api/packages/import/$folderId?restart_recipes=true"
-        Invoke-WebRequest -Uri $uri -Method 'POST' -Form $requestFile -Headers $headers
+        Invoke-RestMethod -Uri $uri -Method 'POST' -Headers $headers -Body $requestFile -ContentType "multipart/form-data"
         Write-Host "manifestName $manifestName"
     }
     else {
@@ -39,5 +39,3 @@ if ($manifestName -ne 'null' -AND (Test-Path $manifestNameFolder )) {
 else {
     Write-Host "Either manifestName is 'null' or $manifestNameFolder does not exist."
 }
-
-
