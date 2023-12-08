a07e3c5a4102263c1cc79db78212d470c031f248
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-08 19:19:55 +0530

    Update import.ps1

--- import.ps1
+++ import.ps1
@@ -3,13 +3,17 @@ Param (
   [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token
   [Parameter(mandatory = $true)][string]$manifestName, # To receive manifest name
   [Parameter(mandatory = $true)][string]$action, # To receive type of action script shall perform
-  [Parameter(mandatory = $true)][string]$folderId # To receive folder ID
+  [Parameter(mandatory = $true)][string]$folderId, # To receive folder ID
+  [Parameter(mandatory = $true)][string]$summary_file_name
 )
 
 $headers = @{ 'Authorization' = "Bearer $accessToken" }
 
 $manifestDirectory = "cicd"
 Write-Host "manifestDirectory:$manifestDirectory"
+
+# Initialize an empty string to store all environment summaries
+$allSummaries_Log = ""
 
 if ($action -eq "Create") {
   Set-Location $manifestDirectory
@@ -22,12 +26,15 @@ if ($action -eq "Create") {
   $zipFile = Get-ChildItem -Filter "$manifestName.zip"
   Write-Host "FileName:$zipFile"
 
+  $allSummaries_Log += $manifestName
+
   if ($zipFile) {
     # Read the ZIP file as byte array
     $fileContent = [System.IO.File]::ReadAllBytes($zipFile)
 
     Write-Host "Found ZIP file: $zipFile"
-    
+    Write-Host "Start Import manifest for $manifestName"
+
     # Upload the ZIP file content to Workato
     Write-Host "Uploading ZIP file content to $uri..."
     $uri = "https://www.workato.com/api/packages/import/"+$folderId+"?restart_recipes=true"
@@ -45,36 +52,60 @@ if ($action -eq "Create") {
   }
 }
 elseif ($action -eq "ImportAll") {
-  Write-Host "Entered into IMPORTALL...!"
+  # Initialize an array to store proxy names
+  $manifestName_Success = @()
+  $manifestName_Failure = @()
+  $manifestNameCountIn_Success = 0
+  $manifestNameCountIn_Failed = 0
+
   Set-Location $manifestDirectory
   $currentdir = Get-Location
-  $manifestNameFolder = "$currentdir"
-  Set-Location $manifestNameFolder
   $zipFiles = Get-ChildItem -Filter "*.zip"
 
   foreach ($zipFile in $zipFiles) {
-    Write-Host "Getting each ZIP FILE...!"
-    # Read the ZIP file as byte array
     $fileContent = [System.IO.File]::ReadAllBytes($zipFile)
 
     Write-Host "Found ZIP file: $zipFile"
-    
+    # Sample filename
+    $filename = $zipFile
+
+    # Extracting the word before ".zip"
+    $extractedWord = $filename -replace '\.zip$', ''
+
+    $manifestName_Success += $extractedWord
+
     # Upload the ZIP file content to Workato
-    Write-Host "Uploading ZIP file content to $uri..."
     $uri = "https://www.workato.com/api/packages/import/"+$folderId+"?restart_recipes=true"
     Write-Host "API:$uri"
 
     try {
       Invoke-RestMethod -Uri $uri -Method "POST" -Headers $headers -Body $fileContent -ContentType "application/zip"
-
-      Write-Host "manifestName $manifestName"
+      Write-Host "manifestName $($zipFile.BaseName)"
     } catch {
-      Write-Host "Error uploading ZIP file: $($_.Exception.Message)"
+      $manifestName_Failure += $extractedWord
+      Write-Host "Error uploading ZIP file $($zipFile.BaseName): $($_.Exception.Message)"
     }
   }
+
+  $manifestNameList_Success =  $($manifestName_Success -join ', ')
+  $manifestNameList_Failed =  $($manifestName_Failure -join ', ')
+
+  $manifestNameCountIn_Success = $manifestName_Success.Count
+  $manifestNameCountIn_Failed = $manifestName_Failure.Count
+
+  $manifestName_Log_Success = ("manifest Recipes Imported Successfully to Workato: Count - $manifestNameCountIn_Success, Manifest Names - $manifestNameList_Success`r`n")
+  $manifestName_Log_Failed = ("manifest Recipes Import Failed: Count - $manifestNameCountIn_Failed, Manifest Names - $manifestNameList_Failed`r`n")
+
+  $allSummaries_Log += $manifestName_Log_Success + $manifestName_Log_Failed
 
 }
 
 else{
   Write-Host "Please atleast one action to perform...!"
 }
+
+# Combine the current directory path with the file name
+$filePath = Join-Path -Path $PWD -ChildPath $summary_file_name
+
+# Write the combined summaries to the summary file
+$allSummaries_Log | Out-File -FilePath $filePath -Append -Encoding UTF8
