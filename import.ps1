9b55ddbe144a3107af57b73e58914ec5f106a74d
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-08 06:01:33 +0530

    Create import.ps1

---
+++ import.ps1
@@ -0,0 +1,37 @@
+# Input parameters
+Param (
+    [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token
+    [Parameter(mandatory = $true)][string]$folderId, # To receive folderId  
+    [Parameter(mandatory = $true)][string]$manifestName # To receive manifest Name  
+)
+
+$headers = @{Authorization = "Bearer $accessToken " }
+
+$manifestDirectory = "cicd"
+
+Write-Host "Start Import manifest for $manifestName "
+Set-Location $manifestDirectory
+$currentdir = Get-Location
+$manifestNameFolder = "$currentdir/$manifestName"
+
+# https://www.workato.com/api/packages/import/#{_('data.lookup_table.f64a4d0d.entry.col3')}?restart_recipes=true
+
+if ($manifestName -ne 'null' -AND (Test-Path $manifestNameFolder )) {
+    Set-Location $manifestNameFolder
+    $zipFile = Get-ChildItem -Filter "$manifestName.zip"
+
+    if ($zipFile) {
+        $requestFile = @{file = Get-Item -Path $zipFile.FullName }
+        $uri = "https://www.workato.com/api/packages/import/$folderId?restart_recipes=true"
+        Invoke-WebRequest -Uri $uri -Method Post -Form $requestFile -Headers $headers
+        Write-Host "manifestName $manifestName"
+    }
+    else {
+        Write-Host "No zip file found with the name $manifestName"
+    }
+}
+else {
+    Write-Host "Either manifestName is 'null' or $manifestNameFolder does not exist."
+}
+
+
