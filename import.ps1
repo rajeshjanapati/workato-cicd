204bf8bee4f05c9c2e6a6fdd9a6430a14ec4e556
rajeshjanapati <67915938+rajeshjanapati@users.noreply.github.com>
2023-12-08 12:35:47 +0530

    Update import.ps1

--- import.ps1
+++ import.ps1
@@ -1,38 +1,88 @@
 # Input parameters
 Param (
-    [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token  
-    [Parameter(mandatory = $true)][string]$manifestName, # To receive manifest Name
-    [Parameter(mandatory = $true)][string]$folderId # To receive folderId
+  [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token
+  [Parameter(mandatory = $true)][string]$manifestName, # To receive manifest name
+  [Parameter(mandatory = $true)][string]$folderId # To receive folder ID
 )
 
 $headers = @{ 'Authorization' = "Bearer $accessToken" }
 
-$manifestDirectory = "cicd"
-Write-Host "manifestDirectory:$manifestDirectory"
+$uri = "https://www.workato.com/api/packages/import/$folderId?restart_recipes=true"
 
-Write-Host "Start Import manifest for $manifestName "
-Set-Location $manifestDirectory
-$currentdir = Get-Location
-$manifestNameFolder = "$currentdir"
+# Check if the ZIP file exists in the current directory
+$zipFile = Get-ChildItem -Filter "$manifestName.zip"
 
-# https://www.workato.com/api/packages/import/#{_('data.lookup_table.f64a4d0d.entry.col3')}?restart_recipes=true
+if ($zipFile) {
+  # Read the ZIP file as byte array
+  $fileContent = [System.IO.File]::ReadAllBytes($zipFile.FullName)
 
-if ($manifestName -ne 'null' -AND (Test-Path $manifestNameFolder)) {
-    Set-Location $manifestNameFolder
-    $zipFile = Get-ChildItem -Filter "$manifestName.zip"
+  Write-Host "Found ZIP file: $zipFile.FullName"
+  Write-Host "Start Import manifest for $manifestName"
 
-    if ($zipFile) {
-        $zipContent = [Convert]::ToBase64String([IO.File]::ReadAllBytes($zipFile.FullName))
-        $requestFile = @{ file = $zipContent; filename = $zipFile.Name }
+  # Upload the ZIP file content to Workato
+  Write-Host "Uploading ZIP file content to $uri..."
 
-        $uri = "https://www.workato.com/api/packages/import/$folderId?restart_recipes=true"
-        Invoke-RestMethod -Uri $uri -Method 'POST' -Headers $headers -Body $requestFile -ContentType "multipart/form-data"
-        Write-Host "manifestName $manifestName"
-    }
-    else {
-        Write-Host "No zip file found with the name $manifestName"
-    }
+  try {
+    Invoke-RestMethod -Uri $uri -Method "POST" -Headers $headers -Body $fileContent -ContentType "application/zip"
+
+    Write-Host "manifestName $manifestName"
+  } catch {
+    Write-Host "Error uploading ZIP file: $($_.Exception.Message)"
+  }
+} else {
+  Write-Host "No ZIP file found with the name $zipFileName"
 }
-else {
-    Write-Host "Either manifestName is 'null' or $manifestNameFolder does not exist."
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Input parameters
+# Param (
+#     [Parameter(mandatory = $true)][string]$accessToken, # To receive Workato token  
+#     [Parameter(mandatory = $true)][string]$manifestName, # To receive manifest Name
+#     [Parameter(mandatory = $true)][string]$folderId # To receive folderId
+# )
+
+# $headers = @{ 'Authorization' = "Bearer $accessToken" }
+
+# $manifestDirectory = "cicd"
+# Write-Host "manifestDirectory:$manifestDirectory"
+
+# Write-Host "Start Import manifest for $manifestName "
+# Set-Location $manifestDirectory
+# $currentdir = Get-Location
+# $manifestNameFolder = "$currentdir"
+
+# # https://www.workato.com/api/packages/import/#{_('data.lookup_table.f64a4d0d.entry.col3')}?restart_recipes=true
+
+# if ($manifestName -ne 'null' -AND (Test-Path $manifestNameFolder)) {
+#     Set-Location $manifestNameFolder
+#     $zipFile = Get-ChildItem -Filter "$manifestName.zip"
+
+#     if ($zipFile) {
+#         $zipContent = [Convert]::ToBase64String([IO.File]::ReadAllBytes($zipFile.FullName))
+#         $requestFile = @{ file = $zipContent; filename = $zipFile.Name }
+
+#         $uri = "https://www.workato.com/api/packages/import/$folderId?restart_recipes=true"
+#         Invoke-RestMethod -Uri $uri -Method 'POST' -Headers $headers -Body $requestFile -ContentType "multipart/form-data"
+#         Write-Host "manifestName $manifestName"
+#     }
+#     else {
+#         Write-Host "No zip file found with the name $manifestName"
+#     }
+# }
+# else {
+#     Write-Host "Either manifestName is 'null' or $manifestNameFolder does not exist."
+# }
